24c65fabdb6ff0f7f2c4e44d13cb8f87b68333c0
Manish Chand <manishtochand@gmail.com>
2023-01-10 09:17:04 +0530

    Update settings.py

--- ProjectHub/settings.py
+++ ProjectHub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@3od6v@_u=r_f3d6u82!#@v@30_!giuvd+^6j*sb^o__mkq0q6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'project-hub-production.up.railway.app']
 
 
 # Application definition
